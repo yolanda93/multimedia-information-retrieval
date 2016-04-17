7b3caef9db7caeb31737b82137028ec81c83347c
Renzo Verastegui <renzo.verastegui@IMDEAs-MacBook-Pro-3.local>
2016-04-17 16:25:58 +0200

    Lucene

--- LuceneApplication/src/main/java/upm/master/MultimediaRetrievalApp.java
+++ LuceneApplication/src/main/java/upm/master/MultimediaRetrievalApp.java
@@ -8,49 +8,73 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 
 public class MultimediaRetrievalApp {
+
+	static String indexDir; //= "/home/yolanda/Index";
+	static String dataDir;//= "/home/yolanda/Data";
+	static Indexer indexer;
+	static Searcher searcher;
+
+	public static void main(String[] args) {
+
+		if (args.length==0){
+
+			System.out.println(" - Modes: \n1 (Indexer) \n - dataDir: The directory path with the data to be indexed. \n - indexDir: The directory path to store the indexed data.\n2 (Searcher)\n - indexDir: The directory path where is stored the indexed data. \n - word: The word to search.");
+		}
+		else{
+
+			MultimediaRetrievalApp tester;
+			try {
+				tester = new MultimediaRetrievalApp();
+
+				if (args[0].equals("1")) {
+
+					dataDir= args[1];
+					indexDir=args[2];
+					
+					tester.createIndex();
+
+				}
+
+				else if (args[0].equals("2")) {
+					
+					indexDir=args[1];
+					tester.search(args[2]);
+
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void createIndex() throws IOException{
+		
+		indexer = new Indexer(indexDir);
+		int numIndexed;
+		long startTime = System.currentTimeMillis();	
+		numIndexed = indexer.createIndex(dataDir, new TextFileFilter());
+		long endTime = System.currentTimeMillis();
+		indexer.close();
+		
+		System.out.println(numIndexed+" File indexed, time taken: " +(endTime-startTime)+" ms");		
 	
-   String indexDir = "/home/yolanda/Index";
-   String dataDir = "/home/yolanda/Data";
-   Indexer indexer;
-   Searcher searcher;
+	}
 
-   public static void main(String[] args) {
-	   MultimediaRetrievalApp tester;
-      try {
-         tester = new MultimediaRetrievalApp();
-         tester.createIndex();
-         tester.search("Mohan");
-      } catch (IOException e) {
-         e.printStackTrace();
-      } catch (ParseException e) {
-         e.printStackTrace();
-      }
-   }
+	private void search(String searchQuery) throws IOException, ParseException{
+		
+		searcher = new Searcher(indexDir);
+		long startTime = System.currentTimeMillis();
+		TopDocs hits = searcher.search(searchQuery);
+		long endTime = System.currentTimeMillis();
 
-   private void createIndex() throws IOException{
-      indexer = new Indexer(indexDir);
-      int numIndexed;
-      long startTime = System.currentTimeMillis();	
-      numIndexed = indexer.createIndex(dataDir, new TextFileFilter());
-      long endTime = System.currentTimeMillis();
-      indexer.close();
-      System.out.println(numIndexed+" File indexed, time taken: "
-         +(endTime-startTime)+" ms");		
-   }
-
-   private void search(String searchQuery) throws IOException, ParseException{
-      searcher = new Searcher(indexDir);
-      long startTime = System.currentTimeMillis();
-      TopDocs hits = searcher.search(searchQuery);
-      long endTime = System.currentTimeMillis();
-   
-      System.out.println(hits.totalHits +
-         " documents found. Time :" + (endTime - startTime));
-      for(ScoreDoc scoreDoc : hits.scoreDocs) {
-         Document doc = searcher.getDocument(scoreDoc);
-            System.out.println("File: "
-            + doc.get(LuceneConstants.FILE_PATH));
-      }
-      searcher.close();
-   }
+		System.out.println(hits.totalHits + " documents found. Time :" + (endTime - startTime));
+		for(ScoreDoc scoreDoc : hits.scoreDocs) {
+			Document doc = searcher.getDocument(scoreDoc);
+			System.out.println("File: " + doc.get(LuceneConstants.FILE_PATH));
+		}
+		searcher.close();
+	}
 }
